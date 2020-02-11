f012dfdf8c1080774b05d3cbcde345cc0cbdb989
anil248su <60932225+anil248su@users.noreply.github.com>
2020-02-11 08:19:51 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,43 @@
 const http = require('http');
-const port = process.env.PORT || 3000
+const { parse } = require('querystring');
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>I am ok</h1>');
+    if (req.method === 'POST') {
+        collectRequestData(req, result => {
+            console.log(result);
+            res.end(`Parsed data belonging to ${result.fname}`);
+        });
+    } 
+    else {
+        res.end(`
+            <!doctype html>
+            <html>
+            <body>
+                <form action="/" method="post">
+                    <input type="text" name="fname" /><br />
+                    <input type="number" name="age" /><br />
+                    <input type="file" name="photo" /><br />
+                    <button>Save</button>
+                </form>
+            </body>
+            </html>
+        `);
+    }
 });
+server.listen(3000);
 
-server.listen(port,() => {
-  console.log(`Server running at port `+port);
-});
+function collectRequestData(request, callback) {
+    const FORM_URLENCODED = 'application/x-www-form-urlencoded';
+    if(request.headers['content-type'] === FORM_URLENCODED) {
+        let body = '';
+        request.on('data', chunk => {
+            body += chunk.toString();
+        });
+        request.on('end', () => {
+            callback(parse(body));
+        });
+    }
+    else {
+        callback(null);
+    }
+}
